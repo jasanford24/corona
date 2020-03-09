c1a1b404b07a47115f88386854830e690c61818d
Jesse Sanford <jasanford24@gmail.com>
2020-03-09 15:22:58 -0400

    edited sms message

--- corona/corona.py
+++ corona/corona.py
@@ -66,10 +66,14 @@ def main():
                 if numbers[k] == x[0]:
                     state_case = x[1]
                     state_death = x[2]
-
-            message = 'The total number of deaths in the United States from the corona virus is now ' + \
-                str(corona_value) + " with " + numbers[k] + " having " + state_case + " confirmed cases and " + state_death +\
-                " deaths."
+            if numbers[k] == state_df[0][0]:
+                message = 'The total number of deaths in the United States from the corona virus is now ' + \
+                    str(corona_value) + " with " + numbers[k] + " having the most confirmed cases at " + state_case + " and " + state_death +\
+                    " deaths."
+            else:
+                message = 'The total number of deaths in the United States from the corona virus is now ' + \
+                    str(corona_value) + " with " + numbers[k] + " having " + state_case + " confirmed cases and " + state_death +\
+                    " deaths."
 
             message = twilioCli.messages.create(body=message,
                                                 from_=logins[2],
