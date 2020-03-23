934e717d2345bd42db3df8d1e9ae7f446a5101e2
Jesse Sanford <jasanford24@gmail.com>
2020-03-23 13:02:21 -0400

    Update corona.py

--- corona/corona.py
+++ corona/corona.py
@@ -1,20 +1,17 @@
-#  Pi 4B Version
-
-import logging
 from pickle import dump, load
 from time import localtime, sleep
 
 import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from twilio.rest import Client
 
-logging.basicConfig(filename='debug.log',
-                    filemode='w',
-                    format='%(asctime)s - %(message)s',
-                    datefmt='%d-%b-%y %H:%M:%S')
+collection_list = ['TotalCases', 'TotalDeaths', 'TotalRecovered', 'NewDeaths']
 
 
 def collect_worldometer():
+    global column_heads
     URL = 'https://www.worldometers.info/coronavirus/country/us/'
 
     page = requests.get(URL)
@@ -22,17 +19,23 @@ def collect_worldometer():
 
     results = soup.find(id='usa_table_countries_today')
     test = results.find_all('td')
+    column_heads = results.find_all('th')
 
-    total_cases = test[-7:][-1].text.strip()
-    total_deaths = test[-7:][3].text.strip()
-    total_recovered = test[-7:][-2].text.strip()
-    total_death_change = test[-7:][4].text.strip()
-
+    # Gets number of columns, in case they change the size of the table
+    column_num = len(column_heads)
+    
+    
+    total_stats = []
+    for x in collection_list:
+        total_stats.append(test[-column_num:][column_get(x)].text.strip())
+    
+    
     table = []
     list = []
+
     count = 0
     for x in test:
-        if count < 7:
+        if count < column_num:
             list.append(x.text.strip())
         else:
             table.append(list)
@@ -40,110 +43,92 @@ def collect_worldometer():
             list.append(x.text.strip())
             count = 0
         count += 1
-    return table, total_cases, total_deaths, total_recovered, total_death_change
+    return table, total_stats
 
 
-# Main function that collects the data and sends personalized data to each phone number
-def main(update_count):
+def collect_county_count():
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument("--incognito")
+    options.binary_location = '/Applications/Brave Browser.app/Contents/MacOS/Brave Browser'
+    driver_path = '/Users/noumenari/Documents/Python Projects/chromedriver'
+    baby_driver = webdriver.Chrome(options=options,
+                                   executable_path=driver_path)
+    baby_driver.get(
+        'https://www.nytimes.com/interactive/2020/us/coronavirus-us-cases.html'
+    )
 
-    # Resets update count for each day
-    if localtime()[3] < 20 and update_count != 0:
-        update_count[1] = 0
-        with open('misc.p', 'wb') as file:
-            dump(update_count, file)
+    # Clicks "Show More" button to show all county data
+    try:
+        element = baby_driver.find_element_by_xpath(
+            '//*[@id="coronavirus-us-cases"]/div/div/div[6]/div/button')
+        baby_driver.execute_script("arguments[0].click();", element)
+    except:
+        logging.warning('County data xpath error')
+        emergency()
 
-    # If after 10am, attempt update. If updated, don't attempt to update again until after 8pm
-    if (localtime()[3] == 19 and update_count[1] == 0):
-        sleep(60*59)
-        logins = login()
-        twilioCli = Client(logins[0], logins[1])
+    # Collects county data and transforms it.
+    states = baby_driver.find_elements_by_xpath(
+        '//*[@id="coronavirus-us-cases"]/div/div/div[6]/div/table')
+    county_df = [x for x in states[0].text.split('\n')][1:]
 
-        # Load {phone_number:state} data from pickle
-        with open('numbers.p', 'rb') as pfile:
-            numbers = load(pfile)
-
-        logging.warning('Sending text messages.')
-
-        table, total_cases, total_deaths, total_recovered, total_death_change = collect_worldometer()
-        
-        sleep(60)
-        
-        for k in numbers:
-            for x in table:
-                if x[0] == numbers[k][0]:
-                    state_case_count = x[-1]
-                    state_death_count = x[3]
-                    state_death_change = x[4]
-
-            for x in county_df:
-                if numbers[k][1] in x[len(numbers[k][0]) + 1:]:
-                    county_count = x.split()[-1]
-
-            if state_case_count == '':
-                state_case_count = '0'
-            if state_death_count == '':
-                state_death_count = '0'
-
-            message = 'U.S. Covid-19\nActive Cases: ' + total_cases + \
-                '\nTotal Deaths: ' + total_deaths
-
-            if total_death_change != '':
-                message += ' (+' + total_death_change + ')'
-
-            message += '\nTotal Recovered: ' + total_recovered + '\n' + \
-                numbers[k][0] + ":\nCases: " + state_case_count + "\nDeaths: " + \
-                state_death_count
-
-            if state_death_change != '':
-                message += ' (' + state_death_change + ')'
-
-            message = twilioCli.messages.create(body=message,
-                                                from_=logins[2],
-                                                to=k)
-
-        update_count[1] += 1
-        logging.warning('Messages sent.')
-
-        # Saves updated total death count
-        with open('misc.p', 'wb') as file:
-            dump(update_count, file)
-
-    sleep_amount = localtime()[4]
-    if sleep_amount < 30:
-        sleep(1800 - (sleep_amount * 60) - localtime()[5])
-    elif sleep_amount >= 30:
-        sleep(1800 - ((sleep_amount - 30) * 60) - localtime()[5])
-    main(update_count)
+    baby_driver.close()
+    return county_df
 
 
-# If an error occurs during data collection.
-# Sends me a text and shuts program down.
-def emergency():
-    from sys import exit
+def main():
+    table, total_stats = collect_worldometer()
 
-    logins = login()
+    county_df = collect_county_count()
+
+    with open('login.p', 'rb') as pfile:
+        logins = load(pfile)
+
     twilioCli = Client(logins[0], logins[1])
 
-    message = twilioCli.messages.create(body="Something has gone wrong.",
-                                        from_=logins[2],
-                                        to=logins[3])
-    exit()
+    for k in numbers:
+        for x in table:
+            if x[0] == numbers[k][0]:
+                state_case_count = x[column_get(collection_list[0])]
+                state_death_count = x[column_get(collection_list[1])]
+                state_death_change = x[column_get(collection_list[3])]
 
+        if state_case_count == '':
+            state_case_count = '0'
+        if state_death_count == '':
+            state_death_count = '0'
 
-# Loads previous total death count.
-def past():
-    try:
-        with open('misc.p', 'rb') as pfile:
-            return load(pfile)
-    except:
-        return [0, 0]
+        for x in county_df:
+            if numbers[k][1] in x[len(numbers[k][0]) + 1:]:
+                county_count = x.split()[-1]
 
+        # Craft personalized message
+        message = 'U.S. Covid-19\nTotal Cases: ' + total_stats[0] + \
+            '\nTotal Deaths: ' + total_stats[1]
+        if total_stats[3] != '':
+            message += ' (+' + total_stats[3] + ')'
+        message += '\nTotal Recovered: ' + total_stats[2] + '\n' + \
+            numbers[k][0] + ":\nCases: " + state_case_count + "\nDeaths: " + \
+            state_death_count
+        if state_death_change != '':
+            message += ' (' + state_death_change + ')'
+        message += "\n" + numbers[k][1] + ":\nCases: " + county_count
+        
+        # message = twilioCli.messages.create(body=message,
+        #                                    from_=logins[2],
+        #                                    to=k)
+        
+        #print(message)
+        #print()
+    
+    sleep(86400 - localtime()[5])
+    main()
 
-# Loads API data
-def login():
-    with open('login.p', 'rb') as pfile:
-        return load(pfile)
+def column_get(tem):
+    for x in range(len(column_heads)):
+        if column_heads[x].text == tem:
+            return x
 
 
 if __name__ == '__main__':
-    main(past())
+    main()    
