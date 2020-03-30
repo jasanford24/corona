30ec988626320def0e735d57e1090a5baba01f50
Jesse Sanford <jasanford24@gmail.com>
2020-03-29 22:47:52 -0400

    Update corona.py

--- corona/corona.py
+++ corona/corona.py
@@ -4,11 +4,16 @@ from pickle import dump, load
 from time import localtime, sleep
 
 import pandas as pd
+import requests
+from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from twilio.rest import Client
 
 
+#  Collects data from:
+#  https://coronavirus.1point3acres.com/en
+#  and stores it in a dataframe.
 def collect_data():
     options = Options()
     options.add_argument("--headless")
@@ -22,32 +27,44 @@ def collect_data():
     # Collect State Data
     states = baby_driver.find_elements_by_xpath(
         '//*[@id="map"]/div[2]/div[1]/div[5]')
-    county_df = [x for x in states[0].text.split('\n')][13:]
-    county_df = [x.replace(',', '') for x in county_df if "+" not in x and "%" not in x]
+    county_df = [
+        x.replace(',', '') for x in states[0].text.split('\n')
+        if "+" not in x and "%" not in x
+    ]
+
+    #  Starts collection when it reads "New York" the first state on the list.
+    for x in range(len(county_df)):
+        if county_df[x] == "New York":
+            county_df = county_df[x:]
+            break
+    #  Stores collected state data in a Pandas dataframe
     state_data = pd.DataFrame(
         [county_df[x:x + 3:] for x in range(0, len(county_df), 3)],
         columns=['state', 'cases', 'deaths'])
 
-    data = pd.DataFrame(columns=[
-        'state', 'county', 'county_cases', 'county_deaths'
-    ])
+    #  Creates an empty dataframe for county data
+    data = pd.DataFrame(
+        columns=['state', 'county', 'county_cases', 'county_deaths'])
 
+    #  Loops through each state on the website and stores county data
     for x in range(2, len(state_data) + 1):
+
         # Clicks "Show More" button to show all county data
         element = baby_driver.find_element_by_xpath(
             '//*[@id="map"]/div[2]/div[1]/div[5]/div[' + str(x) +
-            ']/div/span[1]')
+            ']/div[1]/span[1]')
         baby_driver.execute_script("arguments[0].click();", element)
+
         # Collects county data and transforms it.
         states = baby_driver.find_elements_by_xpath(
             '//*[@id="map"]/div[2]/div[1]/div[5]/div[' + str(x) + ']/div[2]')
-        county_df = [y for y in states[0].text.split('\n')]
-        county_df = [y.replace(',', '') for y in county_df if "+" not in y and "%" not in y]
+        county_df = [
+            y.replace(',', '') for y in states[0].text.split('\n')
+            if "+" not in y and "%" not in y
+        ]
         temp_data = pd.DataFrame(
             [county_df[y:y + 3:] for y in range(0, len(county_df), 3)],
-            columns=[
-                'county', 'county_cases', 'county_deaths'
-            ])
+            columns=['county', 'county_cases', 'county_deaths'])
         temp_data['state'] = state_data['state'][x - 2]
         temp_data['state_cases'] = state_data['cases'][x - 2]
         temp_data['state_deaths'] = state_data['deaths'][x - 2]
@@ -55,18 +72,46 @@ def collect_data():
 
     baby_driver.close()
 
-    data['county_deaths'] = [
-        x.replace(',', '') if x is not None else '0'
-        for x in data['county_deaths']
-    ]
-    data['county_cases'] = [
-        x.replace(',', '') if x is not None else '0'
-        for x in data['county_cases']
-    ]
-    data.reset_index(inplace=True, drop=True)
-    return data
+    return data.append(add_bay_area(data), ignore_index=True)
 
 
+# Prior collection does not combine the bay area into one like it does with New York City
+# So added this so San Francisco can have a Bay Area count.
+def bay_area_collection():
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument("--incognito")
+    options.binary_location = '/usr/bin/chromium-browser'
+    driver_path = 'chromedriver'
+    options.binary_location = '/Applications/Brave Browser.app/Contents/MacOS/Brave Browser'
+    driver_path = '/Users/noumenari/Documents/Python Projects/chromedriver'
+    baby_driver = webdriver.Chrome(options=options,
+                                   executable_path=driver_path)
+    baby_driver.get('https://projects.sfchronicle.com/2020/coronavirus-map/')
+
+    states = baby_driver.find_elements_by_xpath(
+        '//*[@id="gatsby-focus-wrapper"]/main/div[2]/section/div[1]')
+
+    # Slightly faster than list comprehension
+    bay_temp = list(
+        map(lambda x: x.replace(',', ''), states[0].text.split('\n')[1::2]))
+
+    baby_driver.close()
+    return bay_temp
+
+
+# Adds Bay Area data to data dataframe
+def add_bay_area(data):
+    test = bay_area_collection()
+    temp = data[data['county'] == 'San Francisco'].reset_index(drop=True)
+    temp['county'] = 'Bay Area'
+    temp['county_cases'] = test[0]
+    temp['county_deaths'] = test[1]
+    return temp
+
+
+#  Practicing Object Programming.
+#  Makes an object for each client and prepares their data for delivery
 class Account:
     def __init__(self, number, state, county):
         self.number = number
@@ -83,30 +128,33 @@ class Account:
         self.county_death_count = '0'
         self.county_new_deaths = '0'
 
+    #  Sets individual client data
     def set_data(self, data, prior):
-        temp_df = data[(data['state'] == self.state)
-                       & (data['county'] == self.county)].reset_index(
-                           drop=True)
-        temp_prior = prior[(prior['state'] == self.state)
+        current_data = data[(data['state'] == self.state)
+                            & (data['county'] == self.county)].reset_index(
+                                drop=True)
+
+        prior_data = prior[(prior['state'] == self.state)
                            & (prior['county'] == self.county)].reset_index(
                                drop=True)
 
-        self.total_cases = sum([int(x) for x in data['county_cases']])
-        self.total_deaths = sum([int(x) for x in data['county_deaths']])
+        self.total_cases = sum([int(x) for x in data['county_cases'][:-1]])
+        self.total_deaths = sum([int(x) for x in data['county_deaths'][:-1]])
         self.total_new_deaths = str(
             int(self.total_deaths) -
-            sum([int(x) for x in prior['county_deaths']]))
+            sum([int(x) for x in prior['county_deaths'][:-1]]))
 
-        self.state_case_count = temp_df['state_cases'][0]
-        self.state_death_count = temp_df['state_deaths'][0]
+        self.state_case_count = current_data['state_cases'][0]
+        self.state_death_count = current_data['state_deaths'][0]
         self.state_new_deaths = str(
-            int(self.state_death_count) - int(temp_prior['state_deaths'][0]))
+            int(self.state_death_count) - int(prior_data['state_deaths'][0]))
 
-        self.county_case_count = temp_df['county_cases'][0]
-        self.county_death_count = temp_df['county_deaths'][0]
+        self.county_case_count = current_data['county_cases'][0]
+        self.county_death_count = current_data['county_deaths'][0]
         self.county_new_deaths = str(
-            int(self.county_death_count) - int(temp_prior['county_deaths'][0]))
+            int(self.county_death_count) - int(prior_data['county_deaths'][0]))
 
+    #  Builds personalized client message
     def build_message(self):
         message = 'Covid-19'
         message += '\nU.S. - ' + f"{int(self.total_cases):,d}"
@@ -128,6 +176,7 @@ class Account:
             message += ' (+' + f"{int(self.county_new_deaths):,d}" + ')'
         self.message = message
 
+    #  Sends client message
     def send_sms(self):
         with open('login.p', 'rb') as pfile:
             logins = load(pfile)
@@ -139,23 +188,7 @@ class Account:
                                   to=self.number)
 
 
-# If an error occurs during data collection.
-# Sends me a text and shuts program down.
-def emergency(mess):
-    from sys import exit
-
-    with open('login.p', 'rb') as pfile:
-        logins = load(pfile)
-
-    twilioCli = Client(logins[0], logins[1])
-
-    message = twilioCli.messages.create(body=mess,
-                                        from_=logins[2],
-                                        to=logins[3])
-    exit()
-
-
-# Used to set activiation time to 8pm
+# Used to set activiation time to 8pmEST
 def calculate_time():
     eight = 20 * 60 * 60
 
