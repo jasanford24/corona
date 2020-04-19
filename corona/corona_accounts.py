b60794ca056e9d615ad8327e6050a28e735f68e4
Jesse Sanford <jasanford24@gmail.com>
2020-04-19 02:10:32 -0400

    Update corona_accounts.py

--- corona/corona_accounts.py
+++ corona/corona_accounts.py
@@ -13,7 +13,6 @@ class Account:
         self.state = state
         self.county = county
 
-    #  Sets individual client data
     def set_data(self, data, prior):
         current_data = data[(data['state'] == self.state)
                             & (data['county'] == self.county)].reset_index(
@@ -25,35 +24,37 @@ class Account:
 
         self.total_cases = sum([int(x) for x in data['county_cases'][:-1]])
         self.total_deaths = sum([int(x) for x in data['county_deaths'][:-1]])
-        self.total_new_deaths = str(
-            int(self.total_deaths) -
-            sum([int(x) for x in prior['county_deaths'][:-1]]))
+        self.total_new_deaths = int(
+            self.total_deaths) - sum(
+            [int(x) for x in prior['county_deaths'][:-1]])
 
         self.state_case_count = current_data['state_cases'][0]
         self.state_death_count = current_data['state_deaths'][0]
-        self.state_new_deaths = str(
-            int(self.state_death_count) - int(prior_data['state_deaths'][0]))
+        self.state_new_deaths = int(
+            self.state_death_count) - int(
+            prior_data['state_deaths'][0])
 
         self.county_case_count = current_data['county_cases'][0]
         self.county_death_count = current_data['county_deaths'][0]
-        self.county_new_deaths = str(
-            int(self.county_death_count) - int(prior_data['county_deaths'][0]))
+        self.county_new_deaths = int(
+            self.county_death_count) - int(
+            prior_data['county_deaths'][0])
         self.build_message()
 
     def build_message(self):
         message = 'US Covid-19'
-        message += f'\nCases: {int(self.total_cases):,}'
-        message += f'\nDeaths: {int(self.total_deaths):,}'
+        message += f'\nCases: {self.total_cases:,}'
+        message += f'\nDeaths: {self.total_deaths:,}'
 
-        if self.total_new_deaths != '0':
-            message += f' (+{int(self.total_new_deaths):,})'
+        if self.total_new_deaths:
+            message += f' (+{self.total_new_deaths:,})'
 
         message += f'\n{self.state}'
-        message += f'\nCases: {int(self.state_case_count):,}'
-        message += f'\nDeaths: {int(self.state_death_count):,}'
+        message += f'\nCases: {self.state_case_count:,}'
+        message += f'\nDeaths: {self.state_death_count:,}'
 
-        if self.state_new_deaths != '0':
-            message += f' (+{int(self.state_new_deaths):,})'
+        if self.state_new_deaths:
+            message += f' (+{self.state_new_deaths:,})'
 
         # If New York, add City for clarity
         if self.county == 'New York':
@@ -61,14 +62,13 @@ class Account:
         else:
             message += f'\n{self.county}'
 
-        message += f'\nCases: {int(self.county_case_count):,}'
-        message += f'\nDeaths: {int(self.county_death_count):,}'
+        message += f'\nCases: {self.county_case_count:,}'
+        message += f'\nDeaths: {self.county_death_count:,}'
 
-        if self.county_new_deaths != '0':
-            message += f' (+{int(self.county_new_deaths):,})'
+        if self.county_new_deaths:
+            message += f' (+{self.county_new_deaths:,})'
         self.message = message
 
-    #  Sends client message
     def send_sms(self):
         twilioCli.messages.create(body=self.message,
                                   from_=TWIL_NUMB,
@@ -78,6 +78,7 @@ class Account:
         return self.message + '\n'
 
 
+# Threw this in here since I already call the login credentials.
 def emergency(error):
     twilioCli.messages.create(body=error,
                               from_=TWIL_NUMB,
